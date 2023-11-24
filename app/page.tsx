f81cbb8581b3c0b632005b41c297809d5396ae80
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-11-24 20:05:54 +0700

    Setup shadcnui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 export default function Home() {
-  return <p>Hotion</p>;
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  )
 }
