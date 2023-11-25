26fd954dbe5855017b6e53c680b14afb3d002ace
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-11-25 20:59:27 +0700

    Add navigation user settings

--- app/(landing)/_components/heroes.tsx
+++ app/(landing)/_components/heroes.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Heroes = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5xl">
-      <div className="flex items-center">
+      <div className="flex items-center gap-20">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
           <Image
             src="/banner.png"
