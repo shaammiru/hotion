306420b1ab9faf88771636e7b7e38039c784ee8c
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-11-28 06:49:14 +0700

    Add image functionality and setup next config

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,43 +1,41 @@
-"use client";
+"use client"
 
-import { useMutation, useQuery } from "convex/react";
-import dynamic from "next/dynamic";
-import { useMemo } from "react";
+import { useMutation, useQuery } from "convex/react"
+import dynamic from "next/dynamic"
+import { useMemo } from "react"
 
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-// import { Toolbar } from "@/components/toolbar";
-// import { Cover } from "@/components/cover";
-import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import Toolbar from "@/components/toolbar"
+import Cover from "@/components/cover"
+import { Skeleton } from "@/components/ui/skeleton"
 
 interface DocumentIdPageProps {
   params: {
-    documentId: Id<"documents">;
-  };
-};
+    documentId: Id<"documents">
+  }
+}
 
-const DocumentIdPage = ({
-  params
-}: DocumentIdPageProps) => {
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   // const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }) ,[]);
 
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId
-  });
+  })
 
-  const update = useMutation(api.documents.update);
+  const update = useMutation(api.documents.update)
 
   const onChange = (content: string) => {
     update({
       id: params.documentId,
       content
-    });
-  };
+    })
+  }
 
   if (document === undefined) {
     return (
       <div>
-        {/* <Cover.Skeleton /> */}
+        <Cover.Skeleton />
         <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
           <div className="space-y-4 pl-8 pt-4">
             <Skeleton className="h-14 w-[50%]" />
@@ -47,25 +45,25 @@ const DocumentIdPage = ({
           </div>
         </div>
       </div>
-    );
+    )
   }
 
   if (document === null) {
     return <div>Not found</div>
   }
 
-  return ( 
+  return (
     <div className="pb-40">
-      {/* <Cover url={document.coverImage} /> */}
+      <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-        {/* <Toolbar initialData={document} /> */}
+        <Toolbar initialData={document} />
         {/* <Editor
           onChange={onChange}
           initialContent={document.content}
         /> */}
       </div>
     </div>
-   );
+  )
 }
- 
-export default DocumentIdPage;
+
+export default DocumentIdPage
